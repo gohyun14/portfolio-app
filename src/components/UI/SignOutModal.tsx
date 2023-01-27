12e21af212471182a56f023a28cd85e0689dfbd0
Gabe Hyun <gabe.hyun@gmail.com>
2023-01-26 19:57:22 -0800

    added desktop layout

---
+++ src/components/UI/SignOutModal.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { signOut } from "next-auth/react";
+import Modal from "./Modal";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import { Dialog } from "@headlessui/react";
+
+type SignOutModalProps = {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const SignOutModal = ({ setOpen }: SignOutModalProps) => {
+  return (
+    <>
+      <Modal setOpen={setOpen}>
+        <div>
+          <div className="mx-auto flex h-14 w-14 items-center justify-center rounded-full bg-red-100">
+            <ExclamationTriangleIcon
+              className="h-8 w-8 text-red-600"
+              aria-hidden="true"
+            />
+          </div>
+          <div className="mt-3 text-center sm:mt-4">
+            <Dialog.Title
+              as="h3"
+              className="text-lg font-medium leading-6 text-gray-900"
+            >
+              Sign out
+            </Dialog.Title>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+                Are you sure you want to sign out?
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+          <button
+            type="button"
+            className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
+            onClick={() => {
+              setOpen(false);
+              void signOut();
+            }}
+          >
+            Deactivate
+          </button>
+          <button
+            type="button"
+            className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
+            onClick={() => setOpen(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default SignOutModal;
